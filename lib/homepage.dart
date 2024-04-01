039623d45562f92e632f62f7ae587f8b1ca5218c
Piyush Lodha <piyush.lodha22@vit.edu>
2024-04-01 20:16:02 +0530

    day2

---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    String name = "piyush";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome $name to  $days days"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-void main()
-{
+import 'package:newflutter/homepage.dart';
+
+void main() {
   runApp(Myapp());
 }
+
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-
-        child: Center(
-          child: Container(
-            child: Text("Welcome"),
-            ),
-        ),
-      )
+      home: Homepage(),
     );
-    
   }
 }
